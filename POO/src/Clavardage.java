94573d4765d1fd7bff546f3799715f85fd6d8d8c
jean-baptiste <jb@debian-jb>
2019-11-28 08:16:22 +0100

    dossier projet poo

---
+++ POO/src/Clavardage.java
@@ -0,0 +1,9 @@
+public class Clavardage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GUI guy = new GUI();
+		guy.displayThings();
+	}
+
+}
---
+++ POO/src/GUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class GUI {
+	public GUI() {
+		System.out.println("Guy a été créé");
+	}
+	public void displayThings() {
+		
+	}
+}
