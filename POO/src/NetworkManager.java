b44c4c60c2f89c37b60bd92b79bee23a20459290
M. Bigue-Colas Remy <rbigue-c@insa-20553>
2019-11-29 13:51:57 +0100

    Methode de connexion et de communication a tester

--- POO/src/NetworkManager.java
+++ POO/src/NetworkManager.java
@@ -7,7 +7,7 @@ public class NetworkManager {
 		}
 		final int port_connexion = 10000;
 		final int port_message = 20000;
-	public boolean connectionResquest(String login) throws Exception {
+	public boolean sendConnectionResquest(String login) throws Exception {
 			//emission de la demande
 			byte addbroadcast[] = {127, 127, 127, 127};
 			InetAddress broadcast = InetAddress.getByAddress(addbroadcast);
@@ -28,19 +28,44 @@ public class NetworkManager {
 		
 		byte adddest[] = {127, 127, 127, 127};
 		InetAddress adressedest = InetAddress.getByAddress(adddest);
-		byte buffer[] = message.getBytes();
-		DatagramPacket dataSend = new DatagramPacket(buffer, buffer.length, broadcast, port_connexion);
 		Socket socket = new Socket(adressedest, port_message);
-		socket.send(dataSend);
-		//réception réponse
-		byte bufferReception[] = new byte[10];
-		DatagramPacket dataReceived = new DatagramPacket(bufferReception, bufferReception.length);
-		socket.receive(dataReceived);
+		ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream()); 
+		oos.writeObject(message);
+		oos.close();
 		socket.close();
-		
-		
+	
 	}
 	
+	public String receiveMessage(ServerSocket servSocket) throws Exception {
+		Socket link = servSocket.accept();
+		ObjectInputStream ios = new ObjectInputStream(link.getInputStream());
+		String message = (String) ios.readObject();
+		link.close();
+		return message;
 		
 	}
+	public void receiveConnexionRequest(String mylogin) throws Exception {
+		
+		
+		byte bufferReception[] = new byte[10];
+		DatagramSocket socket = new DatagramSocket();
+		DatagramPacket dataReceived = new DatagramPacket(bufferReception, bufferReception.length);
+		socket.receive(dataReceived);
+		String login = dataReceived.getData().toString();
+		String answer = "";
+		if (login.equals(mylogin))
+			answer = "no";
+		else
+			answer = "ok";
+		
+		byte buffer[] = answer.getBytes();
+		DatagramPacket dataSend = new DatagramPacket(buffer, buffer.length, dataReceived.getAddress(), port_connexion);
+		
+		socket.send(dataSend);
+		//réception réponse
+
+		socket.close();
+		
+	}
+}
 
